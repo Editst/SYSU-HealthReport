2d20f3b136208222cc73b990d4267c84a84de53e
Editst <39005998+Editst@users.noreply.github.com>
2021-09-25 00:26:07 +0800

    使用在线 ocr api

--- util.py
+++ util.py
@@ -1,29 +1,21 @@
-import requests,json
-def get_img(ocr,driver):
-    """
-    获取最新的一张验证码图片，并返回识别结果
-    :param ocr: 识别验证码库实例
-    :param driver: 驱动实例，获取cookie
-    :return res:识别结果 
-    """
-    cookies=driver.get_cookies()
+import requests
+
+def get_img(driver, rec_url):
+    ''' 调用某位不知名好心人的在线识别验证码后端
+    '''
+    cookies = driver.get_cookies()
     s = requests.Session()
     for cookie in cookies:
         s.cookies.set(cookie['name'], cookie['value'])
 
-    url = "https://cas-443.webvpn.sysu.edu.cn/cas/captcha.jsp"
+    url = "https://cas.sysu.edu.cn/cas/captcha.jsp"
     res =  s.get(url)
-    with open('1.jpg',"wb") as f:
-        f.write(res.content)
-
-    with open('1.jpg', 'rb') as f:  
-        img_bytes = f.read()
-    res = ocr.classification(img_bytes)
-    return res 
-def read_json():
-    """
-    从config.json读取配置文件
-    """
-    with open("config.json",'r') as load_f:
-      load_dict = json.load(load_f)
-    return load_dict
+    files = {'img': ('captcha.jpg', res.content, 'image/jpeg')}
+    r =  requests.post(rec_url, files = files)
+    if len(r.text) == 4:
+        capt = r.text
+        print(f'验证码识别成功：{capt}')
+        return capt
+    else:
+        print(f'识别失败：{r.text}，重试')
+        return False
