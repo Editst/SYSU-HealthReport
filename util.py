a1c70f1e05872dbb0df30ac9da2011b782a9833d
Editst <39005998+Editst@users.noreply.github.com>
2021-10-01 11:31:39 +0800

    增加 server 酱推送

--- util.py
+++ util.py
@@ -40,9 +40,24 @@ def get_img(driver, token):
 
 
 def tgbot_send(token, chatid, message):
-    data = {'chat_id': chatid, 'text': message}
+    data = {'chat_id': chatid, 'text': f'健康申报结果：{message}'}
     try:
         r = requests.post(f'https://api.telegram.org/bot{token}/sendMessage', data = data)
+        if r.status_code == 200:
+            print('发送通知成功')
+        else:
+            print('发送通知失败')
+    except:
+        print('发送通知失败')
+
+
+def wx_send(wxsend_key, message):
+    data = {
+        "title": f'健康申报结果：{message}',
+        "desp": "如遇身体不适、或居住地址发生变化，请及时更新健康申报信息。"
+    }
+    try:
+        r = requests.post(f'https://sctapi.ftqq.com/{wxsend_key}.send', data = data)
         if r.status_code == 200:
             print('发送通知成功')
         else:
