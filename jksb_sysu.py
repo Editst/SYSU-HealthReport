d8f896f047a27c762562074ccc335fd2a2ae3fc4
djh29 <88069504+djh29@users.noreply.github.com>
2022-01-19 23:06:20 +0800

    Update jksb_sysu.py

--- jksb_sysu.py
+++ jksb_sysu.py
@@ -5,7 +5,7 @@ from util import get_img
 from retrying import retry
 
 options = webdriver.FirefoxOptions()
-driver = webdriver.Firefox(service = Service=f"{os.environ['GITHUB_ACTION_PATH']}/geckodriver.exe", options=options)
+driver = webdriver.Firefox(service =Service(f"{os.environ['GITHUB_ACTION_PATH']}/geckodriver.exe"), options=options)
 print("初始化selenium driver完成")
 
 bot_token = os.environ['TG_BOT_TOKEN']
