28367e3c23e06e78b8d772f57cdac787943d9c72
djh29 <88069504+djh29@users.noreply.github.com>
2022-01-22 02:05:08 +0800

    Update jksb_sysu.py

--- jksb_sysu.py
+++ jksb_sysu.py
@@ -1,6 +1,7 @@
 import os, time
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
 from util import get_img
 from retrying import retry
 
@@ -10,7 +11,7 @@ options.add_argument("--disable-gpu")
 service=Service(f"{os.environ['GITHUB_ACTION_PATH']}/geckodriver.exe")
 service.command_line_args()
 service.start()
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Firefox(service=Service(f"{os.environ['GITHUB_ACTION_PATH']}/geckodriver.exe"),options=options)
 print("初始化selenium driver完成")
 
 bot_token = os.environ['TG_BOT_TOKEN']
@@ -29,44 +30,44 @@ def login():
     password = os.environ['PASSWORD']
 
     print("输入用户名密码")
-    driver.find_element_by_xpath('//*[@id="username"]').send_keys(netid)
-    driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
+    driver.find_element(By.XPATH,'//*[@id="username"]').send_keys(netid)
+    driver.find_element(By.XPATH,'//*[@id="password"]').send_keys(password)
 
     print("识别验证码")
     code = get_img(driver, ocr_token)
     print("输入验证码")
-    driver.find_element_by_xpath('//*[@id="captcha"]').send_keys(code)
+    driver.find_element(By.XPATH,'//*[@id="captcha"]').send_keys(code)
 
     # 点击登录按钮
     print("登录信息门户")
-    driver.find_element_by_xpath('//*[@id="fm1"]/section[2]/input[4]').click()
+    driver.find_element(By.XPATH,'//*[@id="fm1"]/section[2]/input[4]').click()
     try:
-        print(driver.find_element_by_xpath('//*[@id="cas"]/div/div[1]/div/div/h2').text)
+        print(driver.find_element(By.XPATH,'//*[@id="cas"]/div/div[1]/div/div/h2').text)
     except:
-        print(driver.find_element_by_xpath('//*[@id="fm1"]/div[1]/span').text)
+        print(driver.find_element(By.XPATH,'//*[@id="fm1"]/div[1]/span').text)
         raise Exception('登陆失败')
 
 # 失败后随机 3-5s 后重试，最多 6 次
 @retry(wait_random_min=3000, wait_random_max=5000, stop_max_attempt_number=6)
 def jksb():
     print('访问健康申报页面')
-    #driver.get("http://jksb.sysu.edu.cn/infoplus/form/XNYQSB/start")
+    driver.get("http://jksb.sysu.edu.cn/infoplus/form/XNYQSB/start")
     time.sleep(10)
     try:
-        number = driver.find_element_by_xpath('//*[@id="title_description"]').text
+        number = driver.find_element(By.XPATH,'//*[@id="title_description"]').text
         print('打开健康申报成功')
     except:
         print('打开健康申报失败')
         raise Exception('打开健康申报失败')
 
     print("点击下一步")
-    driver.find_element_by_xpath('//*[@id="form_command_bar"]/li[1]').click()
+    driver.find_element(By.XPATH,'//*[@id="form_command_bar"]/li[1]').click()
     time.sleep(10)
 
     print("提交健康申报")
-    driver.find_element_by_xpath('//*[@id="form_command_bar"]/li[1]').click()
+    driver.find_element(By.XPATH,'//*[@id="form_command_bar"]/li[1]').click()
     time.sleep(10)
-    result = driver.find_element_by_xpath('//div[8]/div/div[1]/div[2]').text
+    result = driver.find_element(By.XPATH,'//div[10]/div/div[1]/div[2]').text
     print("完成健康申报")
     return f'{number}: {result}'
 
