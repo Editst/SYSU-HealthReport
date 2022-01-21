b6ee145d102c83e02cc62ddfebb5567fd72b216d
djh29 <88069504+djh29@users.noreply.github.com>
2022-01-22 02:43:47 +0800

    Update jksb_sysu.py

--- jksb_sysu.py
+++ jksb_sysu.py
@@ -11,7 +11,7 @@ options.add_argument("--disable-gpu")
 service=Service(f"{os.environ['GITHUB_ACTION_PATH']}/geckodriver.exe")
 service.command_line_args()
 service.start()
-driver = webdriver.Firefox(service=Service(f"{os.environ['GITHUB_ACTION_PATH']}/geckodriver.exe"),options=options)
+driver = webdriver.Firefox(service=Service(options=options)
 print("初始化selenium driver完成")
 
 bot_token = os.environ['TG_BOT_TOKEN']
