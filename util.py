c72224bb54d7942e7155d8f31e40e16cdd1db8a8
Editst <39005998+Editst@users.noreply.github.com>
2022-03-11 13:42:31 +0800

    Update util.py

--- util.py
+++ util.py
@@ -14,29 +14,18 @@ def get_img(driver, token):
     url = "https://cas.sysu.edu.cn/cas/captcha.jsp"
     res =  s.get(url)
     
-    if token.startswith('RECURL'):
-        files = {'img': ('captcha.jpg', res.content, 'image/jpeg')}
-        r =  requests.post(token[6:], files = files)
-        if len(r.text) == 4:
-            capt = r.text
-            print(f'验证码识别成功：{capt}')
-            return capt
-        else:
-            print(f'识别失败：{r.text}，重试')
-            raise Exception('验证码识别失败')
+    files = {'imgfile': ('captcha.jpg', res.content)}
+    r = requests.post(f'http://api.95man.com:8888/api/Http/Recog?Taken={token}&imgtype=1&len=4', 
+        files=files, headers=headers)
+    arrstr = r.text.split('|')
+    # 返回格式：识别ID|识别结果|用户余额
+    if(int(arrstr[0]) > 0):
+        print(f'验证码识别成功')
+        capt = arrstr[1]
+        return capt
     else:
-        files = {'imgfile': ('captcha.jpg', res.content)}
-        r = requests.post(f'http://api.95man.com:8888/api/Http/Recog?Taken={token}&imgtype=1&len=4', 
-            files=files, headers=headers)
-        arrstr = r.text.split('|')
-        # 返回格式：识别ID|识别结果|用户余额
-        if(int(arrstr[0]) > 0):
-            print(f'验证码识别成功：{arrstr[1]}')
-            capt = arrstr[1]
-            return capt
-        else:
-            print(f'识别失败：{arrstr[1]}，重试')
-            raise Exception('验证码识别失败')
+        print(f'识别失败：{arrstr[0]}，重试')
+        raise Exception('验证码识别失败')
 
 
 def tgbot_send(token, chatid, message):
